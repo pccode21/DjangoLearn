7c423718f9725008f6ed5a89fc6d340a64ed28fb
博弈 <16007005@qq.com>
2020-03-25 02:55:34 +0800

    :+1:

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
